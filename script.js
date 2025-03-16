e1f4b1d0c6f1dab0e34d5e33d2dc796da9206b96
sudipta-9 <dsudipta891@gmail.com>
2025-03-16 12:14:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+let words = document.querySelectorAll(".word");
+words.forEach((word) => {
+    let letters = word.textContent.split("");
+    word.textContent = ""; 
+    letters.forEach((letter) => {
+        let span = document.createElement("span"); 
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    }); 
+}); 
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText=()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex===maxWordIndex ? words[0]:words[currentWordIndex+1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i*80);
+    });
+    nextWord.style.opacity = "1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340+i*80);
+    });
+    currentWordIndex = currentWordIndex===maxWordIndex ? 0 : currentWordIndex+1;
+};
+
+changeText();
+setInterval(changeText,3000)
+
+document.addEventListener("DOMContentLoaded", function () {
+    const circles = document.querySelectorAll(".circle");
+
+    circles.forEach((elem) => {
+        let dots = parseInt(elem.getAttribute("data-dots"));
+        let marked = parseInt(elem.getAttribute("data-percent"));
+        let percent = Math.floor((dots * marked) / 100);
+        let points = "";
+        let rotate = 360 / dots;
+
+        for (let i = 0; i < dots; i++) {
+            points += `<div class="points" style="--i:${i}; --root:${rotate}deg"></div>`;
+        }
+
+        elem.innerHTML = points;
+
+        const pointsMarked = elem.querySelectorAll(".points");
+        for (let i = 0; i < percent; i++) {
+            pointsMarked[i].classList.add("marked");
+        }
+    });
+});
