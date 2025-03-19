76d047739350e71a7c83ab645e515dca6e8a2589
sudipta-9 <dsudipta891@gmail.com>
2025-03-19 19:53:49 +0530

    working on footer section

--- script.js
+++ script.js
@@ -60,3 +60,16 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 var mixer = mixitup('.portfolio-gallery');
+
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
+
+function activeMenu() {
+    let len = section.length;
+    while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+    menuLi.forEach(sec => sec.classList.remove('active'));
+    menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
